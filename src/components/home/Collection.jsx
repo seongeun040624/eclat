003a383263d604d5d084e127f3f20c3be4118797
seongeun040624 <seongeun040624@gmail.com>
2026-03-27 14:10:04 +0900

    collection-1

--- src/components/home/Collection.jsx
+++ src/components/home/Collection.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
-
-import Collerction1 from '../../pages/Collection1'
-import Collerction2 from '../../pages/Collection2'
-import Collerction3 from '../../pages/Collection3'
-
-import '../../style/collection.scss'
+import { useNavigate } from 'react-router-dom';
+import '../../style/collection.scss';
 
 const Collection = () => {
+    const navigate = useNavigate();
+
+    const collectionData = [
+        { id: 1, title: 'collection1', img: '/img/per1.png', path: '/Collection1' },
+        { id: 2, title: 'collection2', img: '/img/per2.png', path: '/Collection2' },
+        { id: 3, title: 'collection3', img: '/img/per3.png', path: '/Collection3' },
+    ];
+
     return (
-        <div className='collection'>
+        <section className='collection'>
+            <h2 className='collection-title'>collection</h2>
             
-        </div>
+            <div className='collection-list'>
+                {collectionData.map((item) => (
+                    <div key={item.id} className='collection-item'>
+                        <div className='img-box'>
+                            <img src={item.img} alt={item.title} />
+                        </div>
+                        <h3 className='item-name'>{item.title}</h3>
+                        <button 
+                            className='watch-btn' 
+                            onClick={() => navigate(item.path)}
+                        >
+                            watch product →
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </section>
     );
 };
 
